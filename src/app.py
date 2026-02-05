80fa59564688533d85ed76c9a8a27178623e8e55
Rodney Effah <slashme101@yahoo.com>
2026-02-05 10:58:26 +0100

    adds selfhosted runners

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         "time": datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
         "hostname": socket.gethostname(),
-        "message": "You are doing great human!:):)"})
+        "message": "You are doing great human!:)"})
 
 @app.route('/api/v1/healthz')
 
@@ -19,8 +19,6 @@ def health():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
-
-
 
 # '/api/v1/details'
 # '/api/v1/healthz'
